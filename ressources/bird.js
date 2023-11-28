0f2b619d9243e894d855010f83b504c318f0185a
simon <simon.tessard@gmail.com>
2023-11-28 14:19:01 +0100

    Changed colors

--- ressources/bird.js
+++ ressources/bird.js
@@ -1,6 +1,6 @@
 export default function Bird() {   
 	const geometry = new THREE.SphereGeometry(10, 32, 16 ); 
-    const material = new THREE.MeshPhongMaterial( { color: 0xff6600 } ); 
+    const material = new THREE.MeshPhongMaterial( { color: 0xb20000 } ); 
     this.mesh = new THREE.Mesh( geometry, material );
     this.mesh.castShadow = true;
 	this.mesh.receiveShadow = true;
